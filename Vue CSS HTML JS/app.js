3fd70ff7e4f4a807c566fbd3f6c5f3bc8d5295a7
labarberaC314 <67079651+labarberaC314@users.noreply.github.com>
2024-03-10 15:54:39 -0400

    Updated 3.10.24 4:00pm

---
+++ Vue CSS HTML JS/app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+      return{
+        Title: 'AcmeCab',
+        Author: 'Christopher LaBarbera',
+        Age: '21',
+      }
+    }
+  })
+  app.mount('#app')
