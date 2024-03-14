e8de1e6850b916c098ca0d82c5b9d4beec90f6d0
labarberaC314 <67079651+labarberaC314@users.noreply.github.com>
2024-03-13 23:49:22 -0400

    Updated 3.13.24

--- Vue CSS HTML JS/app.js
+++ Vue CSS HTML JS/app.js
@@ -1,10 +1,17 @@
 const app = Vue.createApp({
     data() {
       return{
-        Title: 'AcmeCab',
+        Title: 'My Location',
         Author: 'Christopher LaBarbera',
-        Age: '21',
+        Age: 21,
+        userInput: 'GOOGLE LOCATION' // This line should input the user's geolocation
+      }
+    },
+    methods: {
+      changeTitle(Title) { //reference HTML Line 16
+        this.Title = Title
       }
     }
   })
+
   app.mount('#app')
